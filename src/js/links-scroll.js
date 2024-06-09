e1054b1e7c749a5b2902e89a17904e522adeac88
Radek Drweski <rdrweski@gmail.com>
2024-06-09 16:23:15 +0100

    modify scroll

--- src/js/links-scroll.js
+++ src/js/links-scroll.js
@@ -1,33 +1,31 @@
-// LINKS UNDERLINE ANIMATION WHEN SCROLLING
 const sections = document.querySelectorAll("#products, #about, #contact");
 const navLinks = document.querySelectorAll(".products, .about, .contact-btn");
 
 window.addEventListener("scroll", () => {
+  const top = window.scrollY; // Get the current scroll position
+
   sections.forEach((sec) => {
-    const top = window.scrollY;
     const offset = sec.offsetTop - 500;
     const height = sec.offsetHeight;
     const id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach((links) => {
-        links.classList.remove("active-navbar-option");
-        document
-          .querySelector("nav a[href*=" + id + "]")
-          .classList.add("active-navbar-option");
+      navLinks.forEach((link) => {
+        link.classList.remove("active-navbar-option");
       });
+
+      const navLink = document.querySelector(`nav a[href*="${id}"]`);
+      if (navLink) {
+        navLink.classList.add("active-navbar-option");
+      }
     }
   });
 });
-// END LINK ANIMATION
 
-// ===================================================
-
-// LINK ANIMATION WHEN CLICKED
 const links = document.querySelectorAll(".left a, .contact a");
 
 links.forEach((link) => {
-  link.addEventListener("click", () => {
+  link.addEventListener("click", function () {
     links.forEach((link) => {
       link.classList.remove("active-navbar-option");
     });
